195e75ceb779229a819ee39cbe203172eeb705a2
adepte-myao <gr4y0tm0r@gmail.com>
2022-11-18 15:39:03 +0700

    GetRandom uses sectionID and certAreaID

--- internal/storage/tasksrepository.go
+++ internal/storage/tasksrepository.go
@@ -22,8 +22,17 @@ func (repo *PostgresTaskRepository) GetRandom(sectionID int32, cerAreaID int32) 
 	tx, _ := repo.db.Begin()
 	defer tx.Rollback()
 
-	err := tx.QueryRow("SELECT id, question, answer "+
-		"FROM tasks LIMIT 1 OFFSET (random() * (SELECT COUNT(*) FROM tasks));",
+	err := tx.QueryRow(`WITH suit_tasks AS (
+		SELECT tasks.id, tasks.question, tasks.answer
+			FROM tasks
+			JOIN tests t on tasks.test_id = t.id
+			JOIN cert_area ca on t.cert_area_id = ca.id
+		WHERE section_id = $1 AND ca.id = $2
+		)
+		SELECT suit_tasks.id, suit_tasks.question, suit_tasks.answer
+			FROM suit_tasks
+		LIMIT 1 OFFSET (random() * (SELECT COUNT(*) FROM suit_tasks));`,
+		sectionID, cerAreaID,
 	).Scan(&task.ID, &task.Question, &task.Answer)
 	if err != nil {
 		return models.Task{}, err
